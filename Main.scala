9f5099b6c93e714e997d6d49304031a31cfc4c20
haakon <ha@hotmail.com>
2019-10-07 19:26:37 +0200

    some changes. Can't remember what

---
+++ Main.scala
@@ -0,0 +1,6 @@
+object Main {
+       def main(args: Array[String]): Unit = {
+       val replay = new Replay();
+       replay.loadFile();
+       }
+}
---
+++ Replay.scala
@@ -0,0 +1,6 @@
+class Replay {
+
+      def loadFile() {
+      	  println("Gonna load a file in this method);
+      }
+}
